a0d05aa4390f035a5af3fb11cf1185ed157a60c9
springboot20 <abbasopeyemi148@gmail.com>
2025-05-16 11:37:56 +0100

    updated account table  list to list all users account and added necessary types for the component

---
+++ src/components/tables/account-tablelist-component.tsx
@@ -0,0 +1,60 @@
+import { AccountType } from '@/types/account';
+import React from 'react';
+import moment from 'moment';
+
+interface Column {
+  header: string;
+  accessor: string;
+  type?: string;
+}
+
+type AccountTableListProps = {
+  columns: Column[];
+  datum: AccountType[];
+  actions?: JSX.Element | ((row: any) => JSX.Element);
+};
+
+export const AccountTableListComponent = ({ datum, columns, actions }: AccountTableListProps) => {
+  console.log(datum, columns);
+
+  return (
+    <table className='!w-full table-aut0 p-1 border-t border-[#D9DCE7] overflow-x-scroll'>
+      <thead>
+        <tr>
+          {React.Children.toArray(
+            columns.map((column) => {
+              return <th className='text-[#272727] capitalize'>{column.header}</th>;
+            })
+          )}
+
+          {actions && <th className='text-[#272727] capitalize'>action</th>}
+        </tr>
+      </thead>
+
+      <tbody>
+        {React.Children.toArray(
+          datum?.map((row) => {
+            return (
+              <tr>
+                {React.Children.toArray(
+                  columns?.map((column) => (
+                    <td>
+                      {column?.type === 'Date'
+                        ? moment(row.account[column.accessor]).format('Do MMMM, YYYY')
+                        : row.account[column.accessor]}
+                    </td>
+                  ))
+                )}
+                {actions && (
+                  <td className='px-2 py-4 border-b text-[#0B2239] min-w-[200px]'>
+                    {typeof actions === 'function' ? actions(row) : actions}
+                  </td>
+                )}
+              </tr>
+            );
+          })
+        )}
+      </tbody>
+    </table>
+  );
+};
--- src/pages/main-app/accounts/accounts.tsx
+++ src/pages/main-app/accounts/accounts.tsx
@@ -1,9 +1,36 @@
+import { useGetUserAccountsQuery } from '@/features/account/account.slice';
 import { AccountForms } from './components/account-forms';
+import { AccountType } from '@/types/account';
+import { AccountTableListComponent } from '@/components/tables/account-tablelist-component';
 
 export default function Accounts() {
+  const { data } = useGetUserAccountsQuery();
+
+  console.log(data);
+
+  const accounts = data?.data?.accounts as AccountType[];
+
+  const columns = [
+    { header: 'id', accessor: '_id' },
+    { header: 'type', accessor: 'type' },
+    { header: 'status', accessor: 'status' },
+    { header: 'date created', accessor: 'createdAt', type: 'Date' },
+    { header: 'actions', accessor: 'actions' },
+  ];
+
   return (
     <section className='py-24 lg:py-[8rem]'>
       <div className='max-w-7xl mx-auto px-4 2xl:px-0'>
+        <div>
+          <header>
+            <h1 className='lg:text-xl font-medium capitalize text-[#152F00]'>account list</h1>
+          </header>
+
+          <div className='mt-4 overflow-x-scroll !w-full'>
+            <AccountTableListComponent columns={columns} datum={accounts} />
+          </div>
+        </div>
+
         <AccountForms />
       </div>
     </section>
