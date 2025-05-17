6ac23a13d6484c74942c3a4978d69ced0f95f38e
springboot20 <abbasopeyemi148@gmail.com>
2025-05-17 13:04:47 +0100

    implemented new component for card page component

--- src/pages/main-app/cards/cards.tsx
+++ src/pages/main-app/cards/cards.tsx
@@ -1,9 +1,10 @@
-
+import {CardForm} from './components/card-form/card-form'
 
 export default function Cards() {
   return (
     <section className="py-24 lg:py-[8rem]">
-      <div className="max-w-7xl mx-auto px-4 2xl:px-0">
+      <div className="max-w-xl mx-auto px-4 2xl:px-0">
+        <CardForm />
               </div>
     </section>
   );
